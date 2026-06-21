3e738ba83ec76ae26fbdff06ca7bb4b70f572b08
xInsanityInRed <gabrielle.allan@outlook.com>
2026-06-22 01:30:06 +0800

    Changed names and property types to fit HourlyWeather class

--- OpenWeatherApp/Models/HourlyWeather.cs
+++ OpenWeatherApp/Models/HourlyWeather.cs
@@ -14,35 +14,27 @@ namespace OpenWeatherApp.Models
         public decimal lon { get; set; }
         public string timezone { get; set; }
         public int timezone_offset { get; set; }
-        public DataList[] data { get; set; }
+        public HourlyWeatherData[] data { get; set; }
         public string prev { get; set; }
         public string next { get; set; }
 
-        public class DataList
+        public class HourlyWeatherData
         {
+            public string dateTime { get; set; }
             public int dt { get; set; }
-            public int temp { get; set; }
+            public float temp { get; set; }
             public float feels_like { get; set; }
-            public int pressure { get; set; }
+            public float pressure { get; set; }
             public int humidity { get; set; }
             public float dew_point { get; set; }
-            public float uvi { get; set; }
             public int clouds { get; set; }
-            public int visibility { get; set; }
+            public float uvi { get; set; }
+            public float visibility { get; set; }
             public float wind_speed { get; set; }
-            public int wind_deg { get; set; }
             public float wind_gust { get; set; }
+            public int wind_deg { get; set; }
+            public float pop { get; set; }
             public WeatherInfo[] weather { get; set; }
-            public string[] alerts { get; set; }
-            public int pop { get; set; }
-        }
-
-        public class WeatherInfo
-        {
-            public int id { get; set; }
-            public string main { get; set; }
-            public string description { get; set; }
-            public string icon { get; set; }
         }
     }
 }
