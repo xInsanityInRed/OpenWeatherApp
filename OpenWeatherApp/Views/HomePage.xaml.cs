4830997382902a70b9231b035777cb596f57e848
xInsanityInRed <gabrielle.allan@outlook.com>
2026-06-23 18:33:14 +0800

    Milestone: Finished HomePage

--- OpenWeatherApp/Views/HomePage.xaml.cs
+++ OpenWeatherApp/Views/HomePage.xaml.cs
@@ -8,6 +8,7 @@ public partial class HomePage : ContentPage
     string currentCityName = "Perth";
     List<Geocode> place;
     CurrentWeather weatherResult;
+    
     float minTemp;
     float maxTemp;
 
@@ -26,12 +27,13 @@ public partial class HomePage : ContentPage
         //Get current location latitude & longitude
         place = await service.TranslateCityToGeocode(currentCityName, service);
         weatherResult = await service.GetWeatherSearchResults(currentCityName, service, place[0]);
-        minTemp, maxTemp = await service.GetMinAndMaxTemperatures(service, place[0]);
+        (minTemp, maxTemp) = await service.GetMinAndMaxTemperatures(service, place[0]);
 
         // Change CurrentWeather items
         weatherResult.data[0].dateTime = timeConverter.ToDateTime(weatherResult.data[0].dt, weatherResult.timezone_offset);
         weatherResult.data[0].cityName = $"{place[0].name}, {place[0].state}";
-        weatherResult.data[0].min_temp, weatherResult.data[0].max_temp = minTemp, maxTemp
+        weatherResult.data[0].min_temp = minTemp;
+        weatherResult.data[0].max_temp = maxTemp;
 
         var iconName = weatherResult.data[0].weather[0].icon;
         weatherResult.data[0].weather[0].icon = convertImages.ConvertToIconFilename(iconName);
