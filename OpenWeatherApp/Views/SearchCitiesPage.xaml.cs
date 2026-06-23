f7e3e5f61156da18a6233916587c248ea945ca2c
xInsanityInRed <gabrielle.allan@outlook.com>
2026-06-23 22:24:55 +0800

    Added Favourite button functionality (not functional)

--- OpenWeatherApp/Views/SearchCitiesPage.xaml.cs
+++ OpenWeatherApp/Views/SearchCitiesPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using OpenWeatherApp.Models;
+﻿using Newtonsoft.Json;
+using OpenWeatherApp.Models;
 using OpenWeatherApp.Services;
+using PCLStorage;
 
 namespace OpenWeatherApp.Views;
 
@@ -8,6 +10,8 @@ public partial class SearchCitiesPage : ContentPage
 	public List<Geocode> place;
     CurrentWeather weatherResult;
     public int searchLimit = 5;
+    WeatherApiService service = new WeatherApiService("b3aa72c0e805f0c66fae53311f9f0d47", "https://api.openweathermap.org/", "metric");
+
     public SearchCitiesPage()
 	{
 		InitializeComponent();
@@ -24,7 +28,7 @@ public partial class SearchCitiesPage : ContentPage
         favouriteButton.IsVisible = true;
         favouriteButton.Text = "♡";
 
-        WeatherApiService service = new WeatherApiService("b3aa72c0e805f0c66fae53311f9f0d47", "https://api.openweathermap.org/", "metric");
+        
         UnixToUtcConverter timeConverter = new();
         SetImagesService convertImages = new();
         List<CurrentWeatherData> listOfWeatherResults = new();
@@ -32,9 +36,9 @@ public partial class SearchCitiesPage : ContentPage
         place = await service.TranslateCityToGeocode(searchBar.Text, service);
         weatherResult = await service.GetWeatherSearchResults(searchBar.Text, service, place[0]);
 
-        weatherResult.data[0].dateTime = timeConverter.ToDateTime(weatherResult.data[0].dt, weatherResult.timezone_offset);
-
         // Change CurrentWeather items
+        weatherResult.data[0].dateTime = timeConverter.ToDateTime(weatherResult.data[0].dt, weatherResult.timezone_offset);
+        weatherResult.data[0].cityName = $"{place[0].name}, {place[0].state}";
         var iconName = weatherResult.data[0].weather[0].icon;
         weatherResult.data[0].weather[0].icon = convertImages.ConvertToIconFilename(iconName);
 
@@ -51,8 +55,42 @@ public partial class SearchCitiesPage : ContentPage
         }
     }
 
-    private void favoriteButton_Clicked(object sender, EventArgs e)
+    private async void favoriteButton_Clicked(object sender, EventArgs e)
     {
         favouriteButton.Text = "♥";
+        // Get city Geocode object from latest collection view item
+        if (CityCollectionSearchResults.ItemsSource != null)
+        {
+            List<CurrentWeatherData> cityData = (List<CurrentWeatherData>)CityCollectionSearchResults.ItemsSource;
+            var location = cityData[0].cityName;
+            var cityName = location.Split(",").First();
+
+            place = await service.TranslateCityToGeocode(cityName, service);
+            Geocode city = place[0];
+
+            // Convert city to JSON string and create file
+            string cityJsonData = JsonConvert.SerializeObject(city);
+
+            string folderName = "OpenWeatherAppData";
+            string favouriteCityJsonFile = "FavouriteCities.json";
+            string contentToSave = cityJsonData;
+            string loadedContent = "";
+
+            IFolder folder = PCLStorage.FileSystem.Current.LocalStorage;
+            folder = await folder.CreateFolderAsync(folderName, CreationCollisionOption.OpenIfExists);
+            IFile file = await folder.CreateFileAsync(favouriteCityJsonFile, CreationCollisionOption.OpenIfExists);
+
+            IFile openFile = await folder.GetFileAsync(favouriteCityJsonFile);
+
+            loadedContent = await openFile.ReadAllTextAsync();
+            await file.WriteAllTextAsync(loadedContent + contentToSave);
+
+        }
+        else
+        {
+            await DisplayAlertAsync("Error", "City cannot be saved", "OK");
+        }
+
+
     }
 }
